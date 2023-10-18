a7cb644a8c7282d99d125345da1f3a014ff2e7ed
Sean Nance <sean_nance@hotmail.com>
2023-10-18 09:15:22 -0700

    created index.html and script.js files

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let timesURL = 'https://api.sunrise-sunset.org/json?';
 
-function bringOnTheSun() {
-    let myLatitude = document.getElementById("latitude").value;
-    let myLongitude = document.getElementById("longitude").value;
-    let myURL = timesURL + "lat=" + myLatitude + "&lng=" + myLongitude + "&date=today";
-    $.ajax(myURL,
+function getSunriseSunset() {
+    let baseURL = "https://api.sunrise-sunset.org/json?";
+    let latitude = document.getElementById("latitude").value;
+    let longitude = document.getElementById("longitude").value;
+    let url = baseURL + "lat=" + latitude + "&lng=" + longitude + "&date=today";
+    $.ajax(url,
         {
-            success: function bringOnTheSun(APIResponse) {
-                let sunUpDown = document.createElement(div);
+            success: function getSunriseSunset(APIResponse) {
+                let sunUpDown = document.createElement('div');
                 sunUpDown.innerHTML = APIResponse.results.sunrise;
                 document.body.appendChild(sunUpDown);
             }
